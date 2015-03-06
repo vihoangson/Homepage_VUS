d41544ba672f4937521ba22bb56be31f0fb92ce9
vihoangson <vihoangson@gmail.com>
2015-03-06 20:34:22 +0700

    Version 1.0

--- VUS/views/option.php
+++ VUS/views/option.php
@@ -1,20 +1,19 @@
-<?php
-print_r($bai);
-echo "<h2>Option</h2>"
-. "<form action='".base_url()."option/save_option"."' method='post' >";
-foreach ($array_option as $key => $value) {	
-	$value_s=$this->db->where("option",$key)->get("option")->result();
-	if($value["type"]=="boolean"){
-		if(isset($value_s[0]->value))
-			$value_m=(int)$value_s[0]->value;
-		else
-			$value_m=0;
-		echo "<b>".$value["title"]." </b>";
-		echo "<input type='radio' name='".$key."' value='1' ".($value_m==1?"checked":"")."> Có ";
-		echo "<input type='radio' name='".$key."' value='0' ".($value_m==0?"checked":"")."> Không ";
-	}	
-	echo "<hr>";	
-        echo "kitten 123";
-}
-echo "<input type='submit' value='sub'></form>";
-
+<?php
+print_r($bai);
+echo "<h2>Option</h2>"
+. "<form action='".base_url()."option/save_option"."' method='post' >";
+foreach ($array_option as $key => $value) {	
+	$value_s=$this->db->where("option",$key)->get("option")->result();
+	if($value["type"]=="boolean"){
+		if(isset($value_s[0]->value))
+			$value_m=(int)$value_s[0]->value;
+		else
+			$value_m=0;
+		echo "<b>".$value["title"]." </b>";
+		echo "<input type='radio' name='".$key."' value='1' ".($value_m==1?"checked":"")."> Có ";
+		echo "<input type='radio' name='".$key."' value='0' ".($value_m==0?"checked":"")."> Không ";
+	}	
+	echo "<hr>";	
+}
+echo "<input type='submit' value='sub'></form>";
+

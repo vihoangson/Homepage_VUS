19b74e85c72b5de10f5c4b56c64702d26d5a6e94
CBA_Santo <santo@cybridge.jp>
2015-09-08 21:49:11 +0900

    update auto click

--- VUS/views/base/footer.php
+++ VUS/views/base/footer.php
@@ -94,8 +94,16 @@
 		<?php
 	}
  ?>
-</script>
-<script>
+// Here You can type your custom JavaScript...
+if(false){
+    $("body").click(function(){
+		if(sessionStorage.getItem("click")==null){
+			window.open("http://news.nhacthugian.top");
+			console.log(sessionStorage.setItem("click","1"));
+		}
+    })
+}
+
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
